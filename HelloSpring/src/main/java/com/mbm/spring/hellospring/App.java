cb5ba52d4ccff72a43741ef3a636e924b52aed9b
MattKaiser <morenobmatias@gmail.com>
2019-07-06 14:23:45 +0100

    Probando setter y constructores

--- HelloSpring/src/main/java/com/mbm/spring/hellospring/App.java
+++ HelloSpring/src/main/java/com/mbm/spring/hellospring/App.java
@@ -1,5 +1,9 @@
 package com.mbm.spring.hellospring;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,17 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	
+    	ApplicationContext appContext = new ClassPathXmlApplicationContext("com/mbm/spring/xml/beans.xml");
+    	
+    	Mundo m = (Mundo) appContext.getBean("mundo");
+    	System.out.println(m.getMensaje());
+    	
+    	//Como la clase ApplicationContext a traves de la clase ClassPathXmlApplicationContext
+    	//no tiene un metodo para ser cerrado, tenemos que hacerlo a traves de la interfaz
+    	//configurable.
+    	
+    	((ConfigurableApplicationContext)appContext).close();
+    	
     }
 }
